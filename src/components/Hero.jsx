5fbc8919228988ce8e8e16ee160fea71379292ca
Stephen Bronnec <stephen@dust.tt>
2025-07-10 09:44:22 +0200

    update hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Welcome to <span className="text-[#915EFF]">Glittery Helmet!</span>
+            This is <span className="text-[#915EFF]">Glittery Helmet!</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             Forensic investigation made simple.
